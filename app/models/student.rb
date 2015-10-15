392d34d76e4f6e8bb6cb1ca1ee2a5770b89ea6cc
zhiyuan0312 <zhiyuan0312@hotmail.com>
2015-10-15 18:53:00 +0800

    updated version

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,18 @@ require_relative '../../db/config'
 
 class Student < ActiveRecord::Base
 # implement your Student model here
+	validates :age, :numericality => {greater_than: 4}
+	validates :email, :uniqueness => true
+	validates :email, :format => {with: /.+@.+[.][^.]{2,}\z/}
+	validates :phone, :format => { :with => /.+\d{3}.+\d{3}.+\d{4}.+/ }
+
+
+	def name
+		name = self.first_name + " " + self.last_name
+	end
+
+	def age
+		now = Date.today
+		age = now.year - self.birthday.year
+	end
 end
--- db/migrate/20121011144238_create_students.rb
+++ db/migrate/20121011144238_create_students.rb
@@ -5,5 +5,16 @@ require_relative '../config'
 class CreateStudents < ActiveRecord::Migration
   def change
     # HINT: checkout ActiveRecord::Migration.create_table
+    create_table :students do |t|
+
+    	t.string :first_name
+    	t.string :last_name
+    	t.string :gender
+    	t.string :email
+    	t.string :phone
+    	t.date :birthday
+
+    	t.timestamps null: false
+    end
   end
 end
